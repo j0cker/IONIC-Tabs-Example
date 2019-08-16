f27baaa7de45aaf5eff9686c7b0bf9511812def4
Manlio Emiliano Terán Ramos <manlioelnum1@hotmail.com>
2019-08-16 15:52:00 -0500

    Lists under tabs

---
+++ src/app/models/lista.model.ts
@@ -0,0 +1,21 @@
+import { ListaItem } from "./lista-item.model";
+
+export class Lista {
+
+    id:number;
+    titulo:string;
+    creadoEn:Date;
+    terminadoEn:Date;
+    terminada:boolean;
+    items: ListaItem[];
+
+    constructor(titulo:string){
+
+        this.titulo = titulo;
+        this.creadoEn = new Date();
+        this.terminada = false;
+        this.items = [];
+
+    }
+
+}
--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Lista } from "../models/lista.model";
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DeseosService {
 
-  constructor() { }
+  listas:Lista[] = [];
+
+  constructor() { 
+    console.log("[Servicios]");
+
+    const lista1 = new Lista("Recolectar piedras del infinito");
+    const lista2 = new Lista("Héroes a desaparecer");
+
+    this.listas.push(lista1, lista2);
+
+    console.log(this.listas);
+
+  }
 }
